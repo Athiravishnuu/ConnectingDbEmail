e88ce972e8630faa2b700f978f29c02acb1bc35e
Athira Parameswaran s <athira007p@gmail.com>
2023-02-27 08:47:28 +0530

    Add project files.

---
+++ ConnectingDbEmail/Db/DemoDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace ConnectingDbEmail.Db
+{
+    public class DemoDbContext:DbContext
+    {
+
+
+        public DbSet<Model.Email> Emails { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            var path = "Server=localhost\\SQLEXPRESS;Database=Emails;Trusted_Connection=SSPI;Encrypt=false;TrustServerCertificate=true";
+            optionsBuilder.UseSqlServer(path);
+        }
+    }
+
+   
+    
+    
+}
